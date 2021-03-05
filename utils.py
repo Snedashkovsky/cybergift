a35137a0634537b114a5067d0b55b4ec1bb03692
Snedashkovsky <snedashkovsky@gmail.com>
2021-03-05 18:02:38 +0800

    - Update BigQuery utils

--- utils.py
+++ utils.py
@@ -1,18 +1,45 @@
 from config import DATASET_NAME, bq_client, bqstorage_client
 
 
-def drop_table(table_name):
-    table_ref_1 = bq_client.dataset(DATASET_NAME).table(table_name)
-    bq_client.delete_table(table_ref_1)  # API request
-    print('Table {}:{} deleted.'.format(DATASET_NAME, table_name))
+def drop_table(table_name, dataset_name=DATASET_NAME):
+    """
+    Drop a table in a dataset from your project
+    :param table_name: The name of the table to be dropped
+    :param dataset_name: The dataset name in which the dropped table is located
+    :return: True if successful or False otherwise.
+    """
+    table_ref_1 = bq_client.dataset(dataset_name).table(table_name)
+    res = bq_client.delete_table(table_ref_1)  # API request
+    for item in res:
+        print(item)
+        if item == '':
+            print('Table {}:{} deleted.'.format(dataset_name, table_name))
+            return True
+        return False
 
 
-def create_table(query, table_name):
+def create_table(query, table_name, dataset_name=DATASET_NAME):
+    """
+    Create a table in a dataset from your project
+    :param query: SQL query to create a table
+    :param table_name: The name of the table to be created
+    :param dataset_name: The dataset name in which the created table is located
+    :return: True if successful or False otherwise.
+    """
     res = bq_client.query(query)
     for item in res:
         print(item)
-    print('Table {}:{} created and filled.'.format(DATASET_NAME, table_name))
+        if item == '':
+            print('Table {}:{} created and filled.'.format(dataset_name, table_name))
+            return True
+        return False
 
 
-def get_df(query):
+def get_df(query, columns):
+    """
+    Get pandas DataFrame by SQL query
+    :param query: SQL query to get data for a DataFrame
+    :param columns: Name of columns in a DataFrame
+    :return: DataFrame
+    """
     return bq_client.query(query).result().to_dataframe(bqstorage_client=bqstorage_client)
