80550c8042c7a383b1f7e2b9a1a11719a64b3af4
Snedashkovsky <snedashkovsky@gmail.com>
2021-03-05 21:06:34 +0800

    - Update Gas Analysis - Update BigQuery utils: Add view creation. - Update config

--- config.py
+++ config.py
@@ -2,10 +2,17 @@ from google.cloud import bigquery, bigquery_storage
 from google.oauth2 import service_account
 
 
-PROJECT_ID = 'cosmic-keep-223223'
-DATASET_NAME = 'gas_analysis'
 INITIAL_TS = '2015-07-30 00:00:00'
 SNAPSHOT_TS = '2021-03-02 00:00:00'
+
+PROJECT_ID = 'cosmic-keep-223223'
+
+GAS_ANALYSIS_DATASET_NAME = 'gas_analysis'
+GAS_SPEND_BY_CONTRACT_TABLE_NAME = 'gas_spend_by_contract'
+GAS_ANALYSIS_DISTRIBUTION_TABLE_NAME = 'gas_analysis_distribution'
+
+FINAL_DATASET_NAME = 'final'
+FINAL_DISTRIBUTION_VIEW_NAME = 'final_distribution'
 
 
 # Construct a BigQuery client object.
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
-from config import DATASET_NAME, bq_client, bqstorage_client
+from google.cloud import bigquery
+
+from config import GAS_ANALYSIS_DATASET_NAME, FINAL_DATASET_NAME, PROJECT_ID, bq_client, bqstorage_client
 
 
-def drop_table(table_name, dataset_name=DATASET_NAME):
+def drop_table(table_name, dataset_name=GAS_ANALYSIS_DATASET_NAME):
     """
     Drop a table in a dataset from your project
     :param table_name: The name of the table to be dropped
@@ -18,7 +20,7 @@ def drop_table(table_name, dataset_name=DATASET_NAME):
     return False
 
 
-def create_table(query, table_name, dataset_name=DATASET_NAME):
+def create_table(query, table_name, dataset_name=GAS_ANALYSIS_DATASET_NAME):
     """
     Create a table in a dataset from your project
     :param query: SQL query to create a table
@@ -35,11 +37,25 @@ def create_table(query, table_name, dataset_name=DATASET_NAME):
     return False
 
 
-def get_df(query, columns):
+def create_view(query, view_name, dataset_name=FINAL_DATASET_NAME):
+    """
+    Create a view in a dataset from your project
+    :param query: SQL query to create a view
+    :param view_name: The name of the table to be created
+    :param dataset_name: The dataset name in which the created view is located
+    :return: True if successful or False otherwise.
+    """
+    view_id = f"{PROJECT_ID}.{dataset_name}.{view_name}"
+    view = bigquery.Table(view_id)
+    view.view_query = query
+    view = bq_client.create_table(view)
+    print(f"Created {view.table_type}: {str(view.reference)}")
+
+
+def get_df(query):
     """
     Get pandas DataFrame by SQL query
     :param query: SQL query to get data for a DataFrame
-    :param columns: Name of columns in a DataFrame
     :return: DataFrame
     """
     return bq_client.query(query).result().to_dataframe(bqstorage_client=bqstorage_client)
